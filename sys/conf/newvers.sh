8f41b981e9a6e15bbf89ca67fba5eb940c73e4b2
Emmanuel Vadot <emmanuel.vadot@dynfi.com>
2020-10-09 14:22:22 +0200

    BUmp DYNFI_VERSION after dynfi-update modification

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -60,7 +60,7 @@ if [ -n "${BRANCH_OVERRIDE}" ]; then
 fi
 RELEASE="${REVISION}-${BRANCH}"
 VERSION="${TYPE} ${RELEASE}"
-DYNFI_VERSION="0.99.006"
+DYNFI_VERSION="0.99.007"
 
 if [ -z "${SYSDIR}" ]; then
     SYSDIR=$(dirname $0)/..
