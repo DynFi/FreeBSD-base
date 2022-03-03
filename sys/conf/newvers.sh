25341dbea2d86a5ae481c8f4c49c52acc9e147a5
Mariusz Zaborski <oshogbo@dynfi.com>
2022-03-03 20:54:37 +0000

    dynfi: get ready for official 1.00 version

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -59,7 +59,7 @@ if [ -n "${BRANCH_OVERRIDE}" ]; then
 fi
 RELEASE="${REVISION}-${BRANCH}"
 VERSION="${TYPE} ${RELEASE}"
-DYNFI_VERSION="0.99.024"
+DYNFI_VERSION="1.00.000"
 
 if [ -z "${SYSDIR}" ]; then
 	SYSDIR=$(dirname $0)/..
