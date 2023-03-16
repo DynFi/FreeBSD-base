4b6b68f23f711663a6c62dac08c93973e3de11bd
Mariusz Zaborski <oshogbo@dynfi.com>
2023-03-16 19:19:50 +0000

    Version 3.00.000

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -60,7 +60,7 @@ if [ -n "${BRANCH_OVERRIDE}" ]; then
 fi
 RELEASE="${REVISION}-${BRANCH}"
 VERSION="${TYPE} ${RELEASE}"
-DYNFI_VERSION="2.00.000"
+DYNFI_VERSION="3.00.000"
 
 if [ -z "${SYSDIR}" ]; then
     SYSDIR=$(dirname $0)/..
