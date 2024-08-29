9fa23d15919f1fc5eb07fa2e8a41a7a73d15137a
Mariusz Zaborski <oshogbo@dynfi.com>
2024-08-29 09:39:49 +0000

    Change versioning

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -59,7 +59,7 @@ if [ -n "${BRANCH_OVERRIDE}" ]; then
 fi
 RELEASE="${REVISION}-${BRANCH}"
 VERSION="${TYPE} ${RELEASE}"
-DYNFI_VERSION="4.00.001"
+DYNFI_VERSION="4.01"
 
 if [ -z "${SYSDIR}" ]; then
 	SYSDIR=$(dirname $0)/..
