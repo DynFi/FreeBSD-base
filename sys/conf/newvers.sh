15c79f846178a3d66ceb9d61a4e9c8ecb7db4739
Mariusz Zaborski <oshogbo@dynfi.com>
2025-02-26 12:37:55 +0000

    Bump version

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -59,7 +59,7 @@ if [ -n "${BRANCH_OVERRIDE}" ]; then
 fi
 RELEASE="${REVISION}-${BRANCH}"
 VERSION="${TYPE} ${RELEASE}"
-DYNFI_VERSION="4.03"
+DYNFI_VERSION="4.04"
 
 if [ -z "${SYSDIR}" ]; then
 	SYSDIR=$(dirname $0)/..
